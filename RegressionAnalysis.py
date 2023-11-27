a3d35f8c58347a3ba9b3b72aba587b7413d8088a
ari <ari44.0709@gmail.com>
2023-11-27 16:16:06 +0900

    finished regression analysis

---
+++ RegressionAnalysis.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+import pandas as pd
+
+listHigh = [153.3,164.9,168.1,151.5,157.8,156.7,161.1]
+listWeight =[45.5,56.0,55.0,52.8,55.6,50.8,56.4]
+listPrediction = []
+listError = []
+
+p = 1
+q = 1
+SumError = 0
+
+for i in range(len(listHigh)):
+    listPrediction.append(p * listHigh[i] + q)
+    listError.append((listWeight[i] - listPrediction[i]) ** 2)
+    SumError += listError[i]
+
+print("---p=1,q=1---")
+print(listPrediction)
+print(listError)
+print(SumError)
+
+
+
+plt.axis([130, 180, 40, 60])
+plt.plot(listHigh, listWeight, 'o')
+plt.show()
+
+print("---p,q最適化---")
+df = pd.DataFrame({'High': listHigh, 'Weight': listWeight})
+
+x = df[['High']]
+y = df[['Weight']]
+
+model_lr = LinearRegression()
+model_lr.fit(x , y)
+
+plt.axis([130, 180, 40, 60])
+plt.plot(x, y, 'o')
+plt.plot(x, model_lr.predict(x), linestyle='solid')
+plt.show()
+
+print("p = %.5f  %.5f" % (model_lr.coef_, model_lr.intercept_))
